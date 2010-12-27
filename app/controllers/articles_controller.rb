6416aafc910f61caa346c412a9f43a5e814c1ce6
Subhash Chandra <TMaYaD@loonyb.in>
2010-12-27 07:13:20 +0530

    added pagination to the articles index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,11 @@
 class ArticlesController < InheritedResources::Base
 protected
   def collection
+    offset = params[:offset].to_i - 5
     @articles ||= end_of_association_chain.
                       where(:parent_id => nil).
-                      order("created_at DESC")
+                      order("created_at DESC").
+                      offset(offset >= 0 ? offset : 0).
+                      limit(10)
   end
 end
